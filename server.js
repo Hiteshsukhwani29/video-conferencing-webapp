55570fe3a6eaf5091b99accbd831a15b1b51ac3e
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2022-02-02 22:12:56 +0530

    Add ejs,uuid

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const { v4: uuid } = require('uuid');
+
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+
+    res.render('room');
+})
+
+server.listen(5000);
